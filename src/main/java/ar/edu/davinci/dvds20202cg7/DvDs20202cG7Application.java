d5bd6e4fc2bc2d8244911efd0314842ba2a83582
mandrake91 <hernan.fernandez1@davinci.edu.ar>
2020-11-16 16:30:14 -0300

    Commit inicial de proyecto final

---
+++ src/main/java/ar/edu/davinci/dvds20202cg7/DvDs20202cG7Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20202cg7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20202cG7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20202cG7Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/davinci/dvds20202cg7/DvDs20202cG7ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20202cg7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20202cG7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
