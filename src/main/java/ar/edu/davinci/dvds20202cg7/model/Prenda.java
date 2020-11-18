75af989a61ec9e9065bd24514270893bb5a5c64f
mandrake91 <hernan.fernandez1@davinci.edu.ar>
2020-11-18 16:17:53 -0300

    prenda repository test y logs

--- src/main/java/ar/edu/davinci/dvds20202cg7/model/Prenda.java
+++ src/main/java/ar/edu/davinci/dvds20202cg7/model/Prenda.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 
 import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
